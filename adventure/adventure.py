4702e21d81eb26484b80ffe2cea3c9066a376bf5
loonz777 <marcis.kravalis@inox.lv>
2018-01-31 22:12:23 +0000

    initial-commit

---
+++ adventure/adventure.py
@@ -0,0 +1,25 @@
+from data import locations
+
+directions = {
+    'west': (-1, 0),
+    'east': (1, 0),
+    'north': (0, -1),
+    'south': (0, 1),
+}
+
+position = (0, 0)
+
+while True:
+    location = locations[position]
+    print('You are at the %s') % location
+
+    valid_directions = {}
+    for k, v in directions.iteritems():
+        possible_position = (position[0]+ v[0], position[1]+ v[1])
+        possible_location = locations.get(possible_location)
+        if possible_location:
+            print("to the %s is a %s") % (k, possible_location)
+            valid_directions[k] = possible_position
+
+    direction = input('which direction do you want to go?\n')
+    position = valid_directions[direction]
